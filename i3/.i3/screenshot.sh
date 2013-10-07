60eaa903a976ab1bbb48cf7eb4406e762c43bc5d
cfi30 <git@cfillion.tk>
2013-10-07 15:07:09 -0400

    i3: cancel screenshot upload if scrot was aborted

--- i3/.i3/screenshot.sh
+++ i3/.i3/screenshot.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 sleep 0.1
 filename=$(scrot -msb ~/Pictures/screenshots/%Y-%m-%d-%H%M%S.png -e 'echo $f')
+[ -z "$filename" ] && exit
+
 xdg-open $filename &
 xterm -hold -title imgur -e zsh -ic "imgur $filename"
 

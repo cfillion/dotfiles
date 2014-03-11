1e473196fbd5f758cefe84e5fc944d957c610072
cfi30 <git@cfillion.tk>
2014-03-11 01:31:38 -0400

    i3: handle failure when unmounting a drive

--- i3/.i3/umount.sh
+++ i3/.i3/umount.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 device=$(ls /media | dmenu -i -p "device:")
-[ "$device" ] && udiskie-umount -de "/media/$device"
+[ "$device" ] || exit
+
+msg=$(udiskie-umount -de "/media/$device" 2>&1)
+[ "$?" -eq 0 ] || notify-send -u critical "$msg"

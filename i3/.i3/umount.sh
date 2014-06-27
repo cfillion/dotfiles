ad9e827254ad13db847e2e2749c7f1e764af25f3
cfi30 <git@cfillion.tk>
2014-06-27 14:41:45 -0400

    xorg: i3: change automounter & remove clipit

--- i3/.i3/umount.sh
+++ i3/.i3/umount.sh
@@ -2,5 +2,5 @@
 device=$(ls /media | dmenu -i -p "device:")
 [ "$device" ] || exit
 
-msg=$(udiskie-umount -de "/media/$device" 2>&1)
+msg=$(umount "/media/$device" 2>&1)
 [ "$?" -eq 0 ] || notify-send -u critical "$msg"

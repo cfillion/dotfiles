6e2480d754c0dc9d3da37d22cfc0a99d5a036d84
cfi30 <git@cfillion.tk>
2014-07-27 22:53:18 -0400

    enhance external youtube player

--- dwb/.config/dwb/userscripts/mpv-youtube.sh
+++ dwb/.config/dwb/userscripts/mpv-youtube.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 # dwb:xyt
 
+proxy="$DWB_PROXY"
+uri="$DWB_URI"
+title="$DWB_TITLE"
+
+notify-send -t 2 "$DWB_TITLE" "Extracting media..."
+
 echo "js \
   f=document.getElementsByTagName('embed')[0]; \
   f.parentNode.removeChild(f);
 " > "$DWB_FIFO"
 
-video=$(youtube-dl -g --proxy "$DWB_PROXY" "$DWB_URI")
-mpv --title="$DWB_TITLE" "$video"
+cookies="/tmp/youtube-cookies-$RANDOM"
+ua=$(youtube-dl --dump-user-agent)
+media=$(youtube-dl -g --cookies "$cookies" --proxy "$proxy" "$uri")
+
+(
+http_proxy=$proxy mpv --quiet --force-window \
+  --user-agent="$ua" --cookies --cookies-file="$cookies" \
+  --title="$DWB_TITLE" "$media"
+rm "$cookies"
+) &

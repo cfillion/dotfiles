e7b7172d29666e4fd4b39977051c2cc35624a1a9
cfi30 <git@cfillion.tk>
2014-07-27 22:58:41 -0400

    dwb: cleanup last commit

--- dwb/.config/dwb/userscripts/mpv-youtube.sh
+++ dwb/.config/dwb/userscripts/mpv-youtube.sh
@@ -5,7 +5,7 @@ proxy="$DWB_PROXY"
 uri="$DWB_URI"
 title="$DWB_TITLE"
 
-notify-send -t 2 "$DWB_TITLE" "Extracting media..."
+notify-send -t 2 "$title" "Extracting media..."
 
 echo "js \
   f=document.getElementsByTagName('embed')[0]; \
@@ -13,12 +13,13 @@ echo "js \
 " > "$DWB_FIFO"
 
 cookies="/tmp/youtube-cookies-$RANDOM"
-ua=$(youtube-dl --dump-user-agent)
-media=$(youtube-dl -g --cookies "$cookies" --proxy "$proxy" "$uri")
+ua="$(youtube-dl --dump-user-agent)"
+media="$(youtube-dl -g --cookies "$cookies" --proxy "$proxy" "$uri")"
 
 (
-http_proxy=$proxy mpv --quiet --force-window \
+http_proxy="$proxy" mpv --quiet --force-window \
   --user-agent="$ua" --cookies --cookies-file="$cookies" \
   --title="$DWB_TITLE" "$media"
+
 rm "$cookies"
 ) &
